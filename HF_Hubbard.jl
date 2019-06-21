60091d107b1eb44ce9fdd663e530086ba71b9918
Olivier Simard <olivier.simard2@usherbrooke.ca>
2019-06-21 13:20:10 -0400

    Including the 2D case. Compatible with the 1D case.

--- HF_Hubbard.jl
+++ HF_Hubbard.jl
@@ -85,9 +85,9 @@ function initGk(kk::Union{Float64,Array{Float64,1}}, qq::Union{Float64,Array{Flo
     return initGkTemp
 end
 
-function Gk(qq::Union{Float64,Array{Float64,1}}, kk::Union{Float64,Array{Float64,1}}, iωn::Complex{Float64}, SE_func::Matrix{Complex{Float64}})
+function Gk(kk::Union{Float64,Array{Float64,1}}, qq::Union{Float64,Array{Float64,1}}, iωn::Complex{Float64}, SE_func::Matrix{Complex{Float64}})
     gkTemp = Matrix{Complex{Float64}}(undef,(2,2)) ## Spin degrees of freedom 
-    if isa(kk,Float64) && isa(qq,Float64)
+    if (isa(kk,Float64) && isa(qq,Float64)) || (isa(kk,Array{Float64,1}) && isa(qq,Array{Float64,1}))
         gkTemp = inv(iωn*II - epsilonk(kk + qq)*II - SE_func)
     else
         throw(ErrorException("Not a type handled. Check Gk function!"))
@@ -97,7 +97,6 @@ function Gk(qq::Union{Float64,Array{Float64,1}}, kk::Union{Float64,Array{Float64
     return gkTemp
 end
 
-
 function FunctWrapper1D(funct::Function, other::Complex{Float64})
     function Inner_funct(k::Float64)
         return funct(k, other)
@@ -106,8 +105,15 @@ function FunctWrapper1D(funct::Function, other::Complex{Float64})
     return Inner_funct
 end
 
+function FunctWrapper2D(funct::Function, other::Complex{Float64})
+    function Inner_funct(k::Array{Float64,1})
+        return funct(k, other)
+    end
+    
+    return Inner_funct
+end
 
-function integrateComplex1D(funct::N, SE_funct::Matrix{Complex{Float64}}, ii::Int64, structModel::HubbardStruct, BoundArr::Union{Array{Float64,1},Array{Array{Float64,1},1}}; Gridk::Int64=80, opt::String="sum") where {N<:Function}
+function integrateComplex(funct::N, SE_funct::Matrix{Complex{Float64}}, ii::Int64, structModel::HubbardStruct, BoundArr::Union{Array{Float64,1},Array{Array{Float64,1},1}}; Gridk::Int64=80, opt::String="sum") where {N<:Function}
     U = structModel.dict_["U"]
     if isa(BoundArr,Array{Float64,1})
         if ii <= 1
@@ -117,24 +123,48 @@ function integrateComplex1D(funct::N, SE_funct::Matrix{Complex{Float64}}, ii::In
             println(ii, " integrate_complex 1D")
             dressed_funct = (kx::Float64, qx::Float64, iωn::Complex{Float64})->1.0*U*funct(kx, qx, iωn, SE_funct)
         end
+    elseif isa(BoundArr,Array{Array{Float64,1},1})
+        if ii <= 1
+            println(ii, " integrate_complex 2D")
+            dressed_funct = (kk::Array{Float64,1}, qq::Array{Float64,1}, iωn::Complex{Float64})->1.0*U*funct(kk, qq, iωn)
+        elseif ii > 1
+            println(ii, " integrate_complex 2D")
+            dressed_funct = (kk::Array{Float64,1}, qq::Array{Float64,1}, iωn::Complex{Float64})->1.0*U*funct(kk, qq, iωn, SE_funct)
+        end
     end
 
     function real_funct(kx::Float64, iωn::Complex{Float64})
         return real(dressed_funct(kx, 0.0, iωn))
     end
 
+    function real_funct(kk::Array{Float64,1}, iωn::Complex{Float64})
+        return real(dressed_funct(kk, [0.0,0.0], iωn))
+    end
+
     function imag_funct(kx::Float64, iωn::Complex{Float64})
         return imag(dressed_funct(kx, 0.0, iωn))
     end
 
+    function imag_funct(kk::Array{Float64,1}, iωn::Complex{Float64})
+        return imag(dressed_funct(kk, [0.0,0.0], iωn))
+    end
+
+    @assert opt == "sum" || opt == "integral"
     if isa(BoundArr,Array{Float64,1})
-        @assert opt == "sum" || opt == "integral"
         if opt == "integral"
             result_real = (remaining_var::Complex{Float64}) -> 2.0*(2.0*pi)^(-1.0)*quadgk(FunctWrapper1D(real_funct,remaining_var), BoundArr[1], BoundArr[2]; rtol=1.5e-4, atol=1.5e-4, maxevals=100_000)[1]
             result_imag = (remaining_var::Complex{Float64}) -> 2.0*(2.0*pi)^(-1.0)*quadgk(FunctWrapper1D(imag_funct,remaining_var), BoundArr[1], BoundArr[2]; rtol=1.5e-4, atol=1.5e-4, maxevals=100_000)[1]
         elseif opt == "sum"
             result_real = (remaining_var::Complex{Float64}) -> 2.0*(Gridk)^(-1.0)*sum(FunctWrapper1D(real_funct,remaining_var), range(BoundArr[1], stop=BoundArr[2], length=Gridk))
             result_imag = (remaining_var::Complex{Float64}) -> 2.0*(Gridk)^(-1.0)*sum(FunctWrapper1D(imag_funct,remaining_var), range(BoundArr[1], stop=BoundArr[2], length=Gridk))
+        end
+    elseif isa(BoundArr,Array{Array{Float64,1},1})
+        if opt == "integral"
+            result_real = (remaining_var::Complex{Float64}) -> 2.0*(2.0*pi)^(-2.0)*hcubature(FunctWrapper2D(real_funct,remaining_var), BoundArr[1], BoundArr[2]; reltol=1.5e-4, abstol=1.5e-4, maxevals=100_000)[1]
+            result_imag = (remaining_var::Complex{Float64}) -> 2.0*(2.0*pi)^(-2.0)*hcubature(FunctWrapper2D(imag_funct,remaining_var), BoundArr[1], BoundArr[2]; reltol=1.5e-4, abstol=1.5e-4, maxevals=100_000)[1]
+        elseif opt == "sum"
+            result_real = (remaining_var::Complex{Float64}) -> 2.0*(Gridk)^(-2.0)*sum(FunctWrapper2D(real_funct,remaining_var), range(BoundArr[1][1], stop=BoundArr[2][1], length=Gridk))
+            result_imag = (remaining_var::Complex{Float64}) -> 2.0*(Gridk)^(-2.0)*sum(FunctWrapper2D(imag_funct,remaining_var), range(BoundArr[1][1], stop=BoundArr[2][1], length=Gridk))
         end
     end
 
--- Main_Hubbard.jl
+++ Main_Hubbard.jl
@@ -27,13 +27,14 @@ using .SuperHF
 
 function integrateComplex1DWrapper(SE_funct::Function, ii::Int64, structModel::SuperHF.Hubbard.HubbardStruct, BoundArr::Array{Float64,1}, Gridk::Int64, opt::String)
     function tmp_funct(funct::Function)
-        return SuperHF.Hubbard.integrateComplex1D(funct::Function, SE_funct, ii, structModel, BoundArr, Gridk, opt)
+        return SuperHF.Hubbard.integrateComplex(funct::Function, SE_funct, ii, structModel, BoundArr, Gridk, opt)
     end
     return tmp_funct
 end
 
 
-function iterationProcess(structModel::SuperHF.Hubbard.HubbardStruct, BoundArr::Union{Array{Float64,1}, Array{Array{Float64,1},1}}; SubLast::Int64=SubLast, Gridk::Int64=80, opt::String="sum")
+function iterationProcess(structModel::SuperHF.Hubbard.HubbardStruct, BoundArr::Union{Array{Float64,1}, Array{Array{Float64,1},1}}; 
+    SubLast::Int64=SubLast, Gridk::Int64=80, opt::String="sum")
     @assert (isa(BoundArr,Array{Array{Float64,1},1}) || isa(BoundArr,Array{Float64,1})) "Only treats the 1D and 2D cases for now! Check iterationProcess(...)."
     N_it = structModel.N_it_
     SE_funct = missing
@@ -47,10 +48,24 @@ function iterationProcess(structModel::SuperHF.Hubbard.HubbardStruct, BoundArr::
             if it <= 1
                 dummyMatrix = Matrix{Complex{Float64}}(undef,(2,2))
                 println(it, " iteration_process 1D ", "Boundaries1D: ", arrBoundaries)
-                push!(Funct_array, SuperHF.Hubbard.integrateComplex1D(SuperHF.Hubbard.initGk, dummyMatrix, it, structModel, arrBoundaries, Gridk=Gridk, opt=opt))
+                push!(Funct_array, SuperHF.Hubbard.integrateComplex(SuperHF.Hubbard.initGk, dummyMatrix, it, structModel, arrBoundaries, Gridk=Gridk, opt=opt))
             elseif it > 1
                 println(it, " iteration_process 1D ", "Boundaries1D: ", arrBoundaries)
-                push!(Funct_array, SuperHF.Hubbard.integrateComplex1D(SuperHF.Hubbard.Gk, dictArray[it-1][1], it, structModel, arrBoundaries, Gridk=Gridk, opt=opt))
+                push!(Funct_array, SuperHF.Hubbard.integrateComplex(SuperHF.Hubbard.Gk, dictArray[it-1][1], it, structModel, arrBoundaries, Gridk=Gridk, opt=opt))
+            end
+            println("Funct_arr: ", Funct_array)
+            dictArray[it] = Funct_array
+        end
+    elseif isa(BoundArr,Array{Array{Float64,1},1})
+        for it in 1:N_it    
+            Funct_array = Array{Array{Complex{Float64},2},1}([]) ## SubLast has got to be devided in two, because there are two boundaries.
+            if it <= 1
+                dummyMatrix = Matrix{Complex{Float64}}(undef,(2,2))
+                println(it, " iteration_process 2D ", "Boundaries2D: ", BoundArr)
+                push!(Funct_array, SuperHF.Hubbard.integrateComplex(SuperHF.Hubbard.initGk, dummyMatrix, it, structModel, BoundArr, Gridk=Gridk, opt=opt))
+            elseif it > 1
+                println(it, " iteration_process 2D ", "Boundaries2D: ", BoundArr)
+                push!(Funct_array, SuperHF.Hubbard.integrateComplex(SuperHF.Hubbard.Gk, dictArray[it-1][1], it, structModel, BoundArr, Gridk=Gridk, opt=opt))
             end
             println("Funct_arr: ", Funct_array)
             dictArray[it] = Funct_array
@@ -67,6 +82,11 @@ Boundaries1D = Array{Float64,1}([-pi,pi])
 qp_array1D = Array{Float64,1}([a for a in range(-pi,stop=pi,length=Grid_K)])
 qpp_array1D = qp_array1D; k_array1D = qp_array1D; kp_array1D = qp_array1D
 
+## Boundaries in k-space for the 2D system
+Boundaries2D = Array{Array{Float64,1},1}([[-pi,-pi],[pi,pi]])
+qp_array2D = Array{Array{Float64,1},1}([[a,b] for a in range(-pi,stop=pi,length=Grid_K) for b in range(-pi,stop=pi,length=Grid_K)])
+qpp_array2D = qp_array2D; k_array2D = qp_array2D; kp_array2D = qp_array2D
+
 function Gk_conv(vals::Union{SuperHF.Hubbard.Integral2D,SuperHF.Hubbard.Integral1D})
     if isa(vals, SuperHF.Hubbard.Integral1D)
         #println("In gk_conv 1D")
@@ -78,29 +98,39 @@ function Gk_conv(vals::Union{SuperHF.Hubbard.Integral2D,SuperHF.Hubbard.Integral
         Gk = inv(vals.iωn*SuperHF.Hubbard.II - SuperHF.Hubbard.epsilonk(vals.qx)*SuperHF.Hubbard.II - summation_subs)
         
         return Gk
+    elseif isa(vals, SuperHF.Hubbard.Integral2D)
+        println("In gk_conv 2D")
+        for idx in 1:div(SubLast,2)
+            c_container[idx] = dictFunct[N_it][idx]
+        end
+        summation_subs = sum(c_container)
+        
+        Gk = inv(vals.iωn*SuperHF.Hubbard.II - SuperHF.Hubbard.epsilonk([vals.qx, vals.qy])*SuperHF.Hubbard.II - summation_subs)
     end
 end
 
-function Lambda1D(HF::SuperHF.Hubbard.HubbardStruct, Gk1::SuperHF.Hubbard.Integral1D, Gk2::SuperHF.Hubbard.Integral1D)
-    #println("IN LAMBDA FUNCTION 1D", "\n")
+function Lambda(HF::SuperHF.Hubbard.HubbardStruct, Gk1::Union{SuperHF.Hubbard.Integral1D,SuperHF.Hubbard.Integral2D}, 
+    Gk2::Union{SuperHF.Hubbard.Integral1D,SuperHF.Hubbard.Integral2D})
+    #println("IN LAMBDA FUNCTION", "\n")
 
     kernel = inv( 1 - dict["U"]*Gk_conv(Gk1)[1,1]*Gk_conv(Gk2)[2,2] )
 
-    #println("kernel value 1D: ", kernel, "\n")
+    #println("kernel value: ", kernel, "\n")
     return kernel
 end
 
-function Susceptibility1D(HF::SuperHF.Hubbard.HubbardStruct, Gk1::SuperHF.Hubbard.Integral1D, Gk2::SuperHF.Hubbard.Integral1D, Gks::Array{SuperHF.Hubbard.Integral1D,1})
-    #println("IN SUSCEPTIBILITY FUNCTION 1D", "\n")
-    sus = Gk_conv(Gks[1])[1,1]*dict["U"]*Lambda1D(HF,Gk1,Gk2)*Gk_conv(Gks[2])[2,2]*Gk_conv(Gks[3])[2,2]*Gk_conv(Gks[4])[1,1]
+function Susceptibility(HF::SuperHF.Hubbard.HubbardStruct, Gk1::Union{SuperHF.Hubbard.Integral1D,SuperHF.Hubbard.Integral2D}, 
+    Gk2::Union{SuperHF.Hubbard.Integral1D,SuperHF.Hubbard.Integral2D}, Gks::Union{Array{SuperHF.Hubbard.Integral1D,1},Array{SuperHF.Hubbard.Integral2D,1}})
+    #println("IN SUSCEPTIBILITY FUNCTION", "\n")
+    sus = Gk_conv(Gks[1])[1,1]*dict["U"]*Lambda(HF,Gk1,Gk2)*Gk_conv(Gks[2])[2,2]*Gk_conv(Gks[3])[2,2]*Gk_conv(Gks[4])[1,1]
 
-    #println("Susceptibility 1D: ", sus, "\n\n")
+    #println("Susceptibility: ", sus, "\n\n")
     return sus
 end
 
+c_container = Vector{Array{Complex{Float64},2}}(undef,div(SubLast,2))
 if Dims == 1
     dictFunct = iterationProcess(model, Boundaries1D, Gridk=Grid_K, opt="integral")
-    c_container = Vector{Array{Complex{Float64},2}}(undef,div(SubLast,2))
     println("Length of function array: ", length(dictFunct[N_it]))
     function main()
         try
@@ -123,12 +153,59 @@ if Dims == 1
                             Gk1 = SuperHF.Hubbard.Integral1D(k, iωn); Gk2 = SuperHF.Hubbard.Integral1D(kp+qp, iωn)
                             Gks1 = SuperHF.Hubbard.Integral1D(k, iωn); Gks2 = SuperHF.Hubbard.Integral1D(kp+q, iωn)
                             Gks3 = SuperHF.Hubbard.Integral1D(kp, iωn); Gks4 = SuperHF.Hubbard.Integral1D(k-q, iωn)
-                            Matsubara_sus = Susceptibility1D(model, Gk1, Gk2, [Gks1,Gks2,Gks3,Gks4])
+                            Matsubara_sus = Susceptibility(model, Gk1, Gk2, [Gks1,Gks2,Gks3,Gks4])
                             k_sum += Matsubara_sus
                         end
                     end
                 end
                 k_sum = 2.0*(1.0/(Grid_K))^3*k_sum ## 2.0 is for the spin
+                push!(Matsubara_array_susceptibility,k_sum)
+                write(f, "$(iωn)"*"\t\t"*"$(k_sum)"*"\n")
+                close(f)
+            end
+            tot_susceptibility = (2.0/model.beta_)^3*sum(Matsubara_array_susceptibility)
+            println("total Susceptibility for q = $(q): ", tot_susceptibility)
+            return tot_susceptibility
+        catch err
+            if typeof(err) == InterruptException
+                println("ALL THE TASKS HAVE BEEN INTERRUPTED","\n")
+            else
+                println("$(err)")
+            end
+        end
+        println("Program terminated. Have a nice day!")
+        return nothing
+    end
+elseif Dims == 2
+    dictFunct = iterationProcess(model, Boundaries2D, Gridk=Grid_K, opt="integral")
+    println("Length of function array: ", length(dictFunct[N_it]))
+    function main()
+        try
+            @assert isa(dictFunct,Dict{Int64,Array{Array{Complex{Float64},2},1}}) "Dictionnary holding self-energies must have a given form. Look inside main function."
+            Matsubara_array_susceptibility = Array{Complex{Float64},1}()
+            q = [0.,0.]
+            @time for (ii,iωn) in enumerate(model.matsubara_grid_)
+                f = open(filename, "a")
+                if ii == 1
+	                write(f, "#N_it "*"$(N_it)"*" q="*"$(q)"*" Gridk "*"$(Grid_K)"*"\n")
+                end
+                k_sum = 0.0+0.0im
+                println("iwn: ", iωn)
+                for qp in qp_array2D
+                    #println("In qp: ", qp)
+                    for k in k_array2D
+                        #println("In k: ", k)
+                        for kp in kp_array2D
+                            #println("In kp: ", kp)
+                            Gk1 = SuperHF.Hubbard.Integral1D(k[1], k[2], iωn); Gk2 = SuperHF.Hubbard.Integral1D(kp[1]+qp[1], kp[2]+qp[2], iωn)
+                            Gks1 = SuperHF.Hubbard.Integral1D(k[1], k[2], iωn); Gks2 = SuperHF.Hubbard.Integral1D(kp[1]+q[1], kp[2]+q[2], iωn)
+                            Gks3 = SuperHF.Hubbard.Integral1D(kp[1], kp[2], iωn); Gks4 = SuperHF.Hubbard.Integral1D(k[1]-q[1], k[2]-q[2], iωn)
+                            Matsubara_sus = Susceptibility(model, Gk1, Gk2, [Gks1,Gks2,Gks3,Gks4])
+                            k_sum += Matsubara_sus
+                        end
+                    end
+                end
+                k_sum = 2.0*(1.0/(Grid_K)^2)^3*k_sum ## 2.0 is for the spin
                 push!(Matsubara_array_susceptibility,k_sum)
                 write(f, "$(iωn)"*"\t\t"*"$(k_sum)"*"\n")
                 close(f)
